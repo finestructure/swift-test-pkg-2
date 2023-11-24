e9945f614c462cc683ce69427dff8410546d4395
Sven A. Schmidt <sas@finestructure.co>
2023-11-24 15:53:41 +0100

    Reference product

--- Package.swift
+++ Package.swift
@@ -19,7 +19,9 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "swift-test-pkg-2",
-            dependencies: []),
+            dependencies: [
+                .product(name: "swift-test-pkg-3", package: "swift-test-pkg-3")
+            ]),
         .testTarget(
             name: "swift-test-pkg-2Tests",
             dependencies: ["swift-test-pkg-2"]),
