6c35a56b248b34510fe9d358740021b00eb82206
Sven A. Schmidt <sas@finestructure.co>
2023-11-24 16:12:20 +0100

    Use from: requirement

--- Package.swift
+++ Package.swift
@@ -12,7 +12,8 @@ let package = Package(
             targets: ["swift-test-pkg-2"]),
     ],
     dependencies: [
-        .package(url: "https://github.com/finestructure/swift-test-pkg-3.git", branch: "dependency-capture")
+        .package(url: "https://github.com/finestructure/swift-test-pkg-3.git",
+                 from: "1.0.0")
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
